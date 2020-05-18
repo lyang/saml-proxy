80c88fdf30d0ba98662ec846c93dca001833fa66
Lin Yang <github@linyang.me>
2020-05-18 00:43:30 -0700

    Add config file and Cookie support

---
+++ hash.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Poorman's deep_symbolize_keys to workaround Sinatra::IndifferentHash
+class Hash
+  def deep_symbolize_keys
+    each_with_object({}) do |(key, value), hash|
+      hash[key.to_sym] = value.is_a?(Hash) ? deep_symbolize_keys(value) : value
+    end
+  end
+end
--- saml_proxy.rb
+++ saml_proxy.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'sinatra/config_file'
+require_relative 'hash'
 
+# Simple Saml2 SSO proxy like oauth2-proxy
 class SamlProxy < Sinatra::Base
+  configure :development do
+    require 'pry'
+    require 'sinatra/reloader'
+    register Sinatra::Reloader
+  end
+
+  configure do
+    register Sinatra::ConfigFile
+    config_file 'config/*'
+
+    use Rack::Session::Cookie, settings.cookie.deep_symbolize_keys.compact
+  end
 end
